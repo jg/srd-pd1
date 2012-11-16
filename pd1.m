171ab3d2be734c808d57fb5bd457c01d64961a6f
Juliusz Gonera <juliusz.gonera@gmail.com>
2012-11-16 19:42:33 +0100

    Fix noise amplitude/probability selection

--- pd1.m
+++ pd1.m
@@ -4,13 +4,13 @@ function pd1()
 global alfa beta gamma N; % parametry modelu
 N = 10; % liczba etapów
 alfa = 0.1;
-beta = 1;
-gamma = 0.05;
+beta = 0.1;
+gamma = 0.5;
 
 etapy = N:-1:1;
 stany = 5000:5000:50000; % saldo na koncie na pocz¹tku etapu
 sterowania = 0:0.01:1; % procentowy udzia³ operacji walutowych w etapie k
-zaklocenia  = [0.9 0.1; 1 0.1; 1.2 0.3; 1.4 0.05; 1.6 0.08; 1.8 0.08; 2 0.08; 2.2 0.08; 2.4 0.08; 2.6 0.08]; % pary: (zak³ócenie, prawdopodobienstwo zaklocenia)
+zaklocenia  = [0.9 0.1;1 0.1; 1.2 0.3; 1.4 0.02; 1.6 0.08; 1.8 0.08; 2 0.08; 2.2 0.08; 2.4 0.08; 2.6 0.08]'; % pary: (zak³ócenie, prawdopodobienstwo zaklocenia)
 
 Jd = zeros(N+1, length(stany)); % optymalne wskazniki jakosci dla etapow i stanów
 Ud = zeros(N, length(stany)); % optymalne sterowania dla etapow i stanów
@@ -21,12 +21,13 @@ for k = etapy
     for u = sterowania      
       J = 0;
       for w = zaklocenia        
-        % stan_nastepny = f(x, w, k);
+        % stan_nastepny = f(x, w, k);        
         J = J + w(2) * (g(x, u, w(1)) + Jd(k+1, xi));
       end
       
       if J >= Jd(k)         
-        Jd(k, xi) = J; Ud(k, xi) = u;        
+        Jd(k, xi) = J; 
+        Ud(k, xi) = u;        
       end
       
     end
@@ -36,6 +37,7 @@ end
 Jd
 Ud
 end
+
 
 
 function y = indeks_stanu(stany, x)
