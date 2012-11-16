e23172e7ae9db35b05107512f8ec649f99de7e77
Juliusz Gonera <juliusz.gonera@gmail.com>
2012-11-16 19:22:29 +0100

    Fix state/control arrays

--- pd1.m
+++ pd1.m
@@ -1,40 +1,46 @@
 function pd1()
 
-% parametry modelu
-global alfa beta gamma N;
+
+global alfa beta gamma N; % parametry modelu
 N = 10; % liczba etapów
-alfa = 0.001;
+alfa = 0.1;
 beta = 1;
-gamma = 0.01;
+gamma = 0.05;
 
 etapy = N:-1:1;
 stany = 5000:5000:50000; % saldo na koncie na pocz¹tku etapu
 sterowania = 0:0.01:1; % procentowy udzia³ operacji walutowych w etapie k
 zaklocenia  = [0.9 0.1; 1 0.1; 1.2 0.3; 1.4 0.05; 1.6 0.08; 1.8 0.08; 2 0.08; 2.2 0.08; 2.4 0.08; 2.6 0.08]; % pary: (zak³ócenie, prawdopodobienstwo zaklocenia)
 
-Jd = zeros(N+1,1); % wskazniki jakosci dla etapow
-Ud = zeros(N,1); % sterowania dla etapow
+Jd = zeros(N+1, length(stany)); % optymalne wskazniki jakosci dla etapow i stanów
+Ud = zeros(N, length(stany)); % optymalne sterowania dla etapow i stanów
 
 for k = etapy  
   for x = stany    
+    xi = indeks_stanu(stany, x);
     for u = sterowania      
       J = 0;
       for w = zaklocenia        
         % stan_nastepny = f(x, w, k);
-        J = J + w(2) * (g(x, u, w(1)) + Jd(k+1));
+        J = J + w(2) * (g(x, u, w(1)) + Jd(k+1, xi));
       end
       
       if J >= Jd(k)         
-        Jd(k) = J; Ud(k) = u;
+        Jd(k, xi) = J; Ud(k, xi) = u;        
       end
       
     end
   end
 end
 
-
+Jd
+Ud
 end
 
+
+function y = indeks_stanu(stany, x)
+y = find(stany == x);
+end
 
 function y = g(x, u, w)
 global alfa beta;
