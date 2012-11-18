821d37c3afd93dd4449317b6550c963e76b8d08f
Juliusz Gonera <juliusz.gonera@gmail.com>
2012-11-18 12:10:26 +0100

    Cleanup, prettify

--- pd1.m
+++ pd1.m
@@ -1,21 +1,21 @@
 function pd1()
 
+% parametry modelu
+global alfa beta gamma N x0 NS; 
+N     = 10; % liczba etapow
+NS    = 200; % liczba symulacji
+x0    = 15000; % poczatkowy stan konta
+alfa  = 0.1;
+beta  = 0.000007;
+gamma = 0.001;
 
-global alfa beta gamma N x0 NS; % parametry modelu
-N = 10; % liczba etapów
-NS = 200; % liczba symulacji
-x0 = 15000; % poczatkowy stan konta
-alfa= 0.1;
-beta= 0.000007;
-gamma=0.001;
+etapy       = N:-1:1;
+stany       = 500:500:300000; % saldo na koncie na poczatku etapu
+sterowania  = 0:0.01:1; % procentowy udzial operacji walutowych w etapie k
+zaklocenia  = [0.5 0.3;1 0.3; 1.4 0.3; 2 0.1]'; % pary: (zaklocenie, prawdopodobienstwo zaklocenia)
 
-etapy = N:-1:1;
-stany = 500:500:300000; % saldo na koncie na poczštku etapu
-sterowania = 0:0.01:1; % procentowy udział operacji walutowych w etapie k
-zaklocenia  = [0.5 0.3;1 0.3; 1.4 0.3; 2 0.1]'; % pary: (zakłócenie, prawdopodobienstwo zaklocenia)
-
-Jd = zeros(N+1, length(stany)); % optymalne wskazniki jakosci dla etapow i stanów
-Ud = zeros(N, length(stany)); % optymalne sterowania dla etapow i stanów
+Jd = zeros(N+1, length(stany)); % optymalne wskazniki jakosci dla etapow i stanow
+Ud = zeros(N, length(stany)); % optymalne sterowania dla etapow i stanow
 
 for k = etapy  
   for x = stany    
@@ -36,10 +36,7 @@ for k = etapy
   end
 end
 
-Jd;
-Ud;
-
-% symulacja
+%%% Symulacja
 
 Js = [];
 for s = 1:NS
@@ -56,30 +53,32 @@ for s = 1:NS
     J = J + g(x, u, w);
   end
   Js = [Js; J];
-  
 end
 
+%%% Wyniki
+
 disp('Sredni wskaznik jakosci wszystkich symulacji');
-Jsr = sum(Js) / numel(Js)
+Jsr = sum(Js) / length(Js)
 disp('Wartosc oczekiwana optymalizacji');
 Jd(1, indeks_stanu(stany, x0))
-disp('Blad miedzy wartoscia oczekiwana z optymalizacji i symulacji');
-blad = (Jsr/Jd(1,indeks_stanu(stany, x0))-1)
-
+disp('Blad miedzy wartoscia oczekiwana z optymalizacji i symulacji (w %)');
+blad = (Jsr/Jd(1,indeks_stanu(stany, x0))-1)*100
 
 % Optymalna polityka
 figure(1);
 surf(Jd(1:N,:))
+
 % Wykres regul decyzyjnych
 figure(2);
 plot(1:length(stany), Ud(:,1:length(stany)))
-Jsrch = [];          % Wskaznik jakosci dla kazdej serii symulacji
+
+% Wskaznik jakosci dla kazdej serii symulacji
+Jsrch = []; 
 for i = 1:NS
     Jsrch = [Jsrch; sum(Js(1:i)/i)];
 end
 figure(3);
 plot(1:numel(Jsrch), Jsrch);
-%plot(1:numel(Js), Js);
 
 end
 
@@ -96,7 +95,6 @@ end
 
 y = zaklocenia(1, i);
 end
-
 
 function y = indeks_stanu(stany, x)
 [val, y] = min(abs(stany - x));
